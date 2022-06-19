ac884255163f8191259be8f4bcf5b252e61146af
Ubaldo PS <ups9703@gmail.com>
2022-06-19 00:33:17 -0500

    Splash Screen

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require("electron");
+const path = require('path')
 app.disableHardwareAcceleration();
 let appWin;
 
@@ -6,17 +7,36 @@ createWindow = () => {
     appWin = new BrowserWindow({
         width: 800,
         height: 600,
+        show: false,
         title: "Angular and Electron",
         resizable: true,
         webPreferences: {
+         //preload: path.join(__dirname, 'preload.js'),
             contextIsolation: false,
-            nodeIntegration: true
+            nodeIntegration: true,
+
         }
     });
 
     appWin.loadURL(`file://${__dirname}/dist/index.html`);
 
     appWin.setMenu(null);
+
+    var splash = new BrowserWindow({
+      width: 500,
+      height: 300,
+      transparent: true,
+      frame: false,
+      alwaysOnTop: true
+    });
+
+    splash.loadFile('splash.html');
+  splash.center();
+  setTimeout(function () {
+    splash.close();
+    appWin.center();
+    appWin.show();
+  }, 5000);
 
     //appWin.webContents.openDevTools();
 
